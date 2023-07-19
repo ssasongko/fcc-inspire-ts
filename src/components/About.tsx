51a2824bece00756576eaa7a706514ba21247d15
Nur Sasongko <kokonur1020@gmail.com>
2023-07-19 16:52:23 +0700

    fix: padding span

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,9 @@
 const About = () => {
   return (
     <div className="text-white text-lg">
-      <p>
-        Nur Sasongko 2023 &copy; | Web Developer | Made with
-        <span className="text-2xl text-red-500">&hearts;</span>
+      <p className="flex gap-1 items-center">
+        Nur Sasongko 2023 &copy; | Web Developer | Made with 
+        <span className="text-2xl text-red-500 pb-0.5">&hearts;</span>
       </p>
     </div>
   )
